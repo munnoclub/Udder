76977863e90332a1d1ae6c8f70d5ccbb62e3cce2
Tom Munno <munnoclub@googlemail.com>
2020-07-20 14:05:03 +0100

    View did dissappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         hey
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        heyaa
+    }
 
 
 }
